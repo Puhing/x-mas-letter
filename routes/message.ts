b5eafcd15ba8c48f14ab2b32e87618f37855f385
Puhing <33rnzla@gmail.com>
2023-12-29 02:48:12 +0900

    Working on Quest 2, need to build audio player in reader page

--- routes/message.ts
+++ routes/message.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import moment from 'moment';
 import fs from 'fs';
+import bodyParser from 'body-parser';
+import path from 'path';
 
 //
 import MySQL from '../MySQL';
@@ -8,9 +10,11 @@ import { upload } from '../util/fileupload';
 import { decrypt } from '../util/encryption';
 //
 const router = express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 const read_db = MySQL.read();
 const db = MySQL.write();
-
+const directory = 'uploads';
 
 router.get('/', async (req, res) => {
     res.render('message', {
@@ -21,10 +25,19 @@ router.get('/', async (req, res) => {
 router.post('/save_voice', upload.single('file'), async (req, res) => {
     console.log(req.body, req.file, '1차 req');
     const { nickname, visit } = req.body;
+    const directory = 'uploads';
+
+    if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory);
+        console.log(`${directory} 디렉토리가 생성되었습니다.`);
+    } else {
+        console.log(`${directory} 디렉토리가 이미 존재합니다.`);
+    }
+
     try {
         const _user = await db.one(`SELECT userId FROM TB_USER WHERE uuid = ?`, [visit]);
         if(_user){
-            let result = await db.query(`INSERT INTO TB_USER_MAILBOX (userId, file, type, addedAt, \`from\`) VALUES (?, ?, 2, NOW(), ?)`, [_user.userId, req.file.path, nickname]);
+            let result = await db.query(`INSERT INTO TB_USER_MAILBOX (userId, content, type, addedAt, \`from\`) VALUES (?, ?, 2, NOW(), ?)`, [_user.userId, req.file.path, nickname]);
             if (result.insertId > 0) {
                 return res.json({ status: 1, msg: 'Success' });
             } else {
@@ -41,6 +54,7 @@ router.post('/save_voice', upload.single('file'), async (req, res) => {
 
 router.post('/save_text', async (req, res) => {
     const { nickname, content, visit } = req.body;
+    console.log(req.body, req.file, '텍스트 req 확인용');
     try {
         const _user = await db.one(`SELECT userId FROM TB_USER WHERE uuid = ?`, [visit]);
         if(_user){
